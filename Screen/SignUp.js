88ff61e2e51ee70fe68e10eb9d7af0a698e41ac7
vsguerrier <vsguerrier@dinasa.ht>
2023-10-12 18:50:27 -0400

    register done !

--- Screen/SignUp.js
+++ Screen/SignUp.js
@@ -5,11 +5,35 @@ import { Input, Spacer } from 'tamagui'
 import { Label } from 'tamagui'
 import { H2, H4 } from 'tamagui'
 import Container from '../Components/Container'
+import axios from 'axios'
+import { URL } from '../const'
 
 const Login = ({ navigation }) => {
 
-    function sign() {
-        navigation.navigate('home')
+    const [email,setEmail] = useState('');
+    const [password,setPassword] = useState('');
+    const [password_confirmation,setPasswordConfirmation] = useState('');
+    const [birthday,setBirthday] = useState('');
+
+    function signUp() {
+
+        console.warn("URL",URL+'/token/');
+
+        axios.post(URL+'/signup/',{email,password,password_confirmation,birthday})
+            .then((response)=>{
+                if(response.status === 200){
+                    try {
+                        navigation.navigate('login');
+                    } catch (error) {
+                        
+                    }
+                }else{
+
+                }
+                // console.warn(response);
+            }).catch((error)=>{
+                console.warn("error",error);
+            })
     }
     
     return (
@@ -24,27 +48,27 @@ const Login = ({ navigation }) => {
             
             <View style={{flex:5,paddingLeft:20,paddingRight:20,height:'100%',flex:1,flexGrow:1,justifyContent:'center'}}>
                 <Label htmlFor="name">Email</Label>
-                <Input />
+                <Input  value={email} onChangeText={(e)=>setEmail(e)}/>
 
                 <Spacer/>
 
                 <Label htmlFor="name">Password</Label>
-                <Input secureTextEntry/>
+                <Input  secureTextEntry value={password} onChangeText={(e)=>setPassword(e)}/>
 
                 <Spacer/>
 
                 <Label htmlFor="name">Password Confirmation</Label>
-                <Input secureTextEntry/>
+                <Input  secureTextEntry value={password_confirmation} onChangeText={(e)=>setPasswordConfirmation(e)}/>
 
                 <Spacer/>
 
                 <Label htmlFor="name">Birthday</Label>
-                <Input />
+                <Input  value={birthday} onChangeText={(e)=>setBirthday(e)}/>
 
                 <Spacer/>
 
                 <Button size="$3" theme="active" color={'$color.blue10Light'} 
-                    onPress={sign}>Sign Up</Button>
+                    onPress={signUp}>Sign Up</Button>
 
                 <Spacer/>
 
